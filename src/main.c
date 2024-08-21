7b5f3f39ddcbc695e5d6a94d1064486febe66d88
samchristywork <106397573+samchristywork@users.noreply.github.com>
2024-08-21 18:47:32 -0500

    feat: Add function to initialize the board object
    This function applies all of the point multipliers to the board object.

--- src/main.c
+++ src/main.c
@@ -98,6 +98,34 @@ void displayBoard(Board *board) {
   }
 }
 
+Board *initializeBoard() {
+  Board *board = malloc(sizeof(Board));
+  for (int i = 0; i < 15; i++) {
+    for (int j = 0; j < 15; j++) {
+      board->cells[i][j].letter = 0;
+      board->cells[i][j].wordMultiplier = 1;
+      board->cells[i][j].letterMultiplier = 1;
+      if (letterMultiplier[i][j] == '2') {
+        board->cells[i][j].letterMultiplier = 2;
+      } else if (letterMultiplier[i][j] == '3') {
+        board->cells[i][j].letterMultiplier = 3;
+      }
+
+      if (wordMultiplier[i][j] == '2') {
+        board->cells[i][j].wordMultiplier = 2;
+      } else if (wordMultiplier[i][j] == '3') {
+        board->cells[i][j].wordMultiplier = 3;
+      }
+    }
+  }
+  return board;
+}
+
 int main() {
   char *wordListFile = "/usr/share/dict/words";
+
+  Board *board = initializeBoard();
+  board->cells[0][1].letter = 'A';
+
+  displayBoard(board);
 }
